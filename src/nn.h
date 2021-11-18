a0cad732eba6ed307fffbf602f55b2ddf32b6560
Jay Honnold <jayhonnold@gmail.com>
2021-11-17 17:45:20 -0700

    Fixes

--- src/nn.h
+++ src/nn.h
@@ -12,21 +12,34 @@ NN* LoadNN(char* path);
 NN* LoadRandomNN();
 void SaveNN(NN* nn, char* path);
 
-INLINE void ReLU(float* v, size_t n) {
+INLINE void ReLU(float* v, const size_t n) {
+  const size_t width = sizeof(__m256) / sizeof(float);
+  const size_t chunks =  n / width;
+
   const __m256 zero = _mm256_setzero_ps();
 
-  for (size_t j = 0; j < n; j += sizeof(__m256) / sizeof(float))
-    _mm256_store_ps(v + j, _mm256_max_ps(zero, _mm256_load_ps(v + j)));
+  __m256* vector = (__m256*) v;
+
+  for (size_t j = 0; j < chunks; j++)
+    vector[j] = _mm256_max_ps(zero, vector[j]);
 }
 
-INLINE float DotProduct(float* v1, float* v2, size_t n) {
-  __m256 r8 = _mm256_setzero_ps();
-  for (size_t j = 0; j < n; j += sizeof(__m256) / sizeof(float)) {
-    const __m256 a = _mm256_load_ps(v1 + j);
-    const __m256 b = _mm256_load_ps(v2 + j);
-    r8 = _mm256_add_ps(r8, _mm256_mul_ps(a, b));
+INLINE float DotProduct(float* v1, float* v2, const size_t n) {
+  const size_t width = sizeof(__m256) / sizeof(float);
+  const size_t chunks =  n / width;
+
+  __m256 s0 = _mm256_setzero_ps();
+  __m256 s1 = _mm256_setzero_ps();
+
+  __m256* vector1 = (__m256*) v1;
+  __m256* vector2 = (__m256*) v2;
+  
+  for (size_t j = 0; j < chunks; j += 2) {
+    s0 = _mm256_add_ps(_mm256_mul_ps(vector1[j], vector2[j]), s0);
+    s1 = _mm256_add_ps(_mm256_mul_ps(vector1[j + 1], vector2[j + 1]), s1);
   }
 
+  const __m256 r8 = _mm256_add_ps(s0, s1);
   const __m128 r4 = _mm_add_ps(_mm256_castps256_ps128(r8), _mm256_extractf128_ps(r8, 1));
   const __m128 r2 = _mm_add_ps(r4, _mm_movehl_ps(r4, r4));
   const __m128 r1 = _mm_add_ss(r2, _mm_shuffle_ps(r2, r2, 0x1));
