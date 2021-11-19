1d5cf8326cb76eb7e20537b62d291508345edf52
Jay Honnold <jayhonnold@gmail.com>
2021-11-19 16:14:49 -0700

    Decay

--- src/gradients.h
+++ src/gradients.h
@@ -6,13 +6,16 @@
 #include "types.h"
 #include "util.h"
 
-INLINE void UpdateAndApplyGradient(float* v, Gradient* grad) {
+INLINE void UpdateAndApplyGradient(float* v, Gradient* grad, const bool decay) {
   if (!grad->g)
     return;
 
   grad->M = BETA1 * grad->M + (1.0 - BETA1) * grad->g;
   grad->V = BETA2 * grad->V + (1.0 - BETA2) * grad->g * grad->g;
   float delta = ALPHA * grad->M / (sqrtf(grad->V) + EPSILON);
+
+  if (decay)
+    *v *= (1.0 - LAMBDA);
 
   *v -= delta;
 
@@ -22,17 +25,17 @@ INLINE void UpdateAndApplyGradient(float* v, Gradient* grad) {
 INLINE void ApplyGradients(NN* nn, NNGradients* g) {
 #pragma omp parallel for schedule(auto) num_threads(THREADS)
   for (int i = 0; i < N_INPUT * N_HIDDEN; i++)
-    UpdateAndApplyGradient(&nn->inputWeights[i], &g->inputWeights[i]);
+    UpdateAndApplyGradient(&nn->inputWeights[i], &g->inputWeights[i], true);
 
 #pragma omp parallel for schedule(auto) num_threads(THREADS)
   for (int i = 0; i < N_HIDDEN; i++)
-    UpdateAndApplyGradient(&nn->inputBiases[i], &g->inputBiases[i]);
+    UpdateAndApplyGradient(&nn->inputBiases[i], &g->inputBiases[i], false);
 
 #pragma omp parallel for schedule(auto) num_threads(THREADS)
   for (int i = 0; i < N_HIDDEN * 2; i++)
-    UpdateAndApplyGradient(&nn->outputWeights[i], &g->outputWeights[i]);
+    UpdateAndApplyGradient(&nn->outputWeights[i], &g->outputWeights[i], true);
 
-  UpdateAndApplyGradient(&nn->outputBias, &g->outputBias);
+  UpdateAndApplyGradient(&nn->outputBias, &g->outputBias, false);
 }
 
 INLINE void ClearGradients(NNGradients* gradients) {
