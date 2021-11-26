4c4c1bae76cf6ae8669ff0e9578bcad36e1110ac
Jay Honnold <jayhonnold@gmail.com>
2021-11-25 21:09:45 -0700

    Try higher squared error
    idea from Sopels NNUE docs

--- src/trainer.h
+++ src/trainer.h
@@ -8,10 +8,12 @@ float TotalError(DataSet* data, NN* nn);
 void Train(int batch, DataSet* data, NN* nn, NNGradients* g, BatchGradients* local);
 
 INLINE float Error(float r, DataEntry* e) {
-  return WDL_WEIGHT * powf(r - e->wdl / 2.0f, 2) + EVAL_WEIGHT * powf(r - e->eval, 2);
+  return WDL_WEIGHT * powf(fabs(r - e->wdl / 2.0f), 2.5f) + EVAL_WEIGHT * powf(fabs(r - e->eval), 2.5f);
 }
+
 INLINE float ErrorGradient(float r, DataEntry* e) {
-  return 2 * WDL_WEIGHT * (r - e->wdl / 2.0f) + 2 * EVAL_WEIGHT * (r - e->eval);
+  return 2.5f * WDL_WEIGHT * (r - e->wdl / 2.0f) * sqrtf(fabs(r - e->wdl / 2.0f)) +
+         2.5f * EVAL_WEIGHT * (r - e->eval) * sqrtf(fabs(r - e->eval));
 }
 
 #endif
